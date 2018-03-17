2aa3041d3b7cc3dad99e14535f3786ed16db01dd
Ke Wang <453587854@qq.com>
2018-03-17 14:37:39 +0800

    revert webpack command to non lean-build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
+const webpack = require("webpack");
 const path = require('path');
-const shrinkWrapJson = require('./npm-shrinkwrap.json');
-const BUILD_DIR = path.join(__dirname, '/lean-build');
+const BUILD_DIR = path.join(__dirname, '/dist');
 
 const PATHS = {
   src: path.join(__dirname, '/src'),
@@ -9,23 +9,17 @@ const PATHS = {
 
 const entry = {};
 entry.app = PATHS.src;
-const externals = {};
-
-Object.keys(shrinkWrapJson.dependencies).forEach(_ => {
-  let ver = shrinkWrapJson.dependencies[_].version;
-  let depName = _ + "_" + ver;
-  externals[_] = {
-    window: ["$deps", depName],
-  };
-});
 
 module.exports = {
   context: __dirname,
   entry,
-  externals,
   output: {
     path: PATHS.dist,
     filename: '[name].js',
     libraryTarget: "window",
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+    })],
 };
